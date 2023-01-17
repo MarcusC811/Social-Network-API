450082b697e1bb5718c4bbd01004691ce4848440
OVOMARK <mcncstatefan@icloud.com>
2023-01-16 21:18:35 -0500

    more model query structuring updates for Thought model

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -3,11 +3,16 @@ const { User, Thought } = require('../../models');
 module.exports = {
     // GET to get all thoughts
     getThoughts() {
-
+        Thought.find().then((thoughts) => res.json(thoughts));
     },
     // GET to get a single thought by its _id
     getSingleThought() {
-
+        Thought.findOne( {_id: req.params.id}
+            .then((thought) => 
+            !thought
+                ? res.status(404).json({ message: "Invalid ID" })
+                : res.json(thought))
+            .catch((err) => res.status(500).json(err)));
     },
     // POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
     createThought() {
@@ -15,17 +20,22 @@ module.exports = {
     },
     // PUT to update a thought by its _id
     updateThought() {
-
+        Thought.findOneandUpdate({_id: req.params.id}, { thoughtText: req.body.thoughtText })
+        .then((thought) => 
+            !thought
+                ? res.status(404).json({message: "No user found with that ID"})
+                : res.json(thought)
+            .catch((err) => res.status(500).json(err)))
     },
     // DELETE to remove a thought by its _id
     deleteThought() {
 
     },
-    // POST to create a reaction stored in a single thought's reactions array field
+    // POST to create a reaction stored in a single thought's reactions array field - thoughtId
     addReaction() {
 
     },
-    // DELETE to pull and remove a reaction by the reaction's reactionId value
+    // DELETE to pull and remove a reaction by the reaction's reactionId value - thoughtId
     deleteReaction() {
 
     },
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -10,38 +10,56 @@ module.exports = {
     // GET a single user by its _id and populated thought and friend data
     getSingleUser() {
         User.findOne({ _id: req.params.id })
-        .select('-__v').then((user) => res.json(user)
-        .catch((err) => res.status(404).json(err)));
+            .select('-__v')
+            .then((user) => 
+                !user ? res.status(404).json({message: "No user found with that ID"})
+                : res.json(user)
+            .catch((err) => res.status(500).json(err)));
     },
     // POST a new user
     postNewUser() {
         User.create(req.body)
-        .then((user) => res.json(user))
-        .catch((err) => res.status(404).json(err));
+        .then((user) =>
+            !user
+                ? res.status(404).json({message: "Invalid submission"})
+                : res.json(user))
+        .catch((err) => res.status(500).json(err));
     },
     // PUT to update a user by its _id
     updateUser() {
         User.findOneAndUpdate({ _id: req.params.id }, {username: req.body.username})
-        .then((user) => res.json(user))
-        .catch((err) => res.status(404).json(err));
+        .then((user) => 
+            !user
+                ? res.status(404).json({message: "Invalid submission"})
+                : res.json(user))
+        .catch((err) => res.status(500).json(err));
     },
     // DELETE to remove user by its _id
     deleteUser() {
         User.findOneAndDelete({ _id: req.params.id })
-            .then((user) => !user ? res.status(404).json( {message: "No user was found"}) : Thought.deleteMany({ _id: { $in: user.thought } }))
+            .then((user) =>
+            !user
+                ? res.status(404).json( {message: "No user was found"})
+                : Thought.deleteMany({ _id: { $in: user.thoughts } }))
             .then(() => res.json( {message: "User was deleted"} ))
             .catch((err) => res.status(500).json(err));
     },
     // POST to add a new friend to a user's friend list
     addFriend() {
-        User.findOneAndUpdate( { _id: req.params.id }, { friends: req.body }
-        .then((user) => res.json(user)
+        User.findOneAndUpdate( { _id: req.params.userId }, { friends: req.body }
+        .then((user) => 
+            !user
+                ? res.status(404).json({message: "No user found with that ID"})
+                : res.json(user)
         .catch((err) => res.status(500).json(err))));
     },
     // DELETE to remove a friend from a user's friend list
     deleteFriend() {
         Course.findOneAndDelete({friends: req.params.friendId})
-        .then((user) => res.json(user))
-        .catch((err) => res.status(404),json(err));
+        .then((user) =>
+            !user
+                ? res.status(404).json("No user found with that ID")
+                : res.json(user))
+        .catch((err) => res.status(500),json(err));
     },
 }
